459fa823832a105fe5add921cb84c99e8fb773cd
essohanam <essohanam.alou@imsp-uac.org>
2023-06-08 23:14:27 +0000

    sever side rendering

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require("express");
 // express app
 const app = express();
+const port = 8000;
 
 app.set("view engine", "ejs");
 app.set("views", "src");
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    { title: "first blog", snippet: "something here to display as blog" },
+    {
+      title: "second blog",
+      snippet: "another thing to display here as content of the blog",
+    },
+  ];
+  res.render("index", { title: "Home", blogs: blogs });
 });
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "about" });
 });
 app.get("/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "new " });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
